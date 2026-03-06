ec22976c5adeb5211fec1effaf80127aaef588f4
noah.guerin <noahguerin20@gmail.com>
2026-03-06 15:33:54 +0100

    first commit

---
+++ src/main/java/com/sportsplatform/api/PickupSportsApiApplication.java
@@ -0,0 +1,13 @@
+package com.sportsplatform.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickupSportsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickupSportsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sportsplatform/api/PickupSportsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportsplatform.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickupSportsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
